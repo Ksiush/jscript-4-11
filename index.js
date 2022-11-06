76685a518e3be3872d078e8df85a8be14151061c
Xenia Golovach <ksiushagolovach@gmail.com>
2022-11-06 15:59:56 +0200

    Use types &create let variable

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert(505)
+console.log(505)
+console.log(5.55)
+console.log('5.55')
+console.log(false)
+console.log(null)
+console.log(undefined)
+
+
+
+let userFirstName='Tom';
+userFirstName = 'Bred';
+userFirstName = "O'Donel";
+userFirstName = 'O\'Donel';
+
+console.log(userFirstName)
+
+let itSunInSky=false;
