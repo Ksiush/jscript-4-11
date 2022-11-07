5bcddfdcf6fcfd5f29fad38b4213736cb8a245d3
Xenia Golovach <ksiushagolovach@gmail.com>
2022-11-07 09:49:39 +0200

    Show changes

--- index.js
+++ index.js
@@ -1,46 +1,16 @@
-console.log(505)
-console.log(5.55)
-console.log('5.55')
-console.log(false)
-console.log(null)
-console.log(undefined)
+let number1;
+console.log(number1);
+number1= 12;
+console.log(number1);
 
+let userFirstName;
 
+const userLastName ='Pit';
 
-let userFirstName='Tom';
-userFirstName = 'Bred';
-userFirstName = "O'Donel";
-userFirstName = 'O\'Donel';
+userFirstName = 'Tom'
 
-console.log(userFirstName)
+//userFirstName - 'Brad'
 
-let itSunInSky=false;
-console.log(itSunInSky)
+//userLastName = 'Cruze'
 
-let carName;
-carName = 'Land Rover';
-carName = 'Volvo';
-carName = 'Mitsubishi';
-
-console.log(carName);
-
-let itHaveACar=true
-
-console.log(itHaveACar)
-
-let userYearsOld =(23);
-userYearsOld='25'
-userYearsOld='23'
-console.log(userYearsOld)
-
-let userAdult=true;
-console.log(userAdult)
-
-let userProgrammingLanguage;
-userProgrammingLanguage='C++'
-
-console.log(userProgrammingLanguage)
-
-let itHaveAPet=false
-
-console.log(itHaveAPet)
+console.log(userFirstName, userLastName)
