6abd3575d027f1d524d774a8efeedb99c6299d59
Xenia Golovach <ksiushagolovach@gmail.com>
2022-11-10 18:20:25 +0200

    Add new homework 10-11

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-(1)    
-let number = 1;
-if ((number <=1)) {
-console.log('positive');
-} else {
-console.log('negative');  
-}
+// 1) написати функцію, яка повертає слово 'odd' або 'even', в залежності від прийнятого аргументу(число).
+// Значення для перевірки функції вводить користувач.
 
-(2)
-const num1 =45;
-const num2 =12;
-if (num1>num2) {
-console.log(num1-num2);    
-} else {
-console.log(num1*num2);    
-}
+const number1 = prompt('Input number');
 
-(3)
-const numb = 63;
-if ((numb % 7 ===0) && (numb%3===0)){
-console.log('you win!');   
-} else {
-console.log ('you lose!');    
+const isNumber = function(number){
+    if (number%2 ===0) {
+        return 'even';
+    }
+         return 'odd'
+    
 }
+ console.log(isNumber(number1));
+
+
+
+
+//  2)написати функцію, яка приймає два аргументи і, якщо аргументи одного типу, то повертає той, який має більше значення (якщо значення рівні - повертаємо null), інакше(якщо різного типу) теж повертає null.
+
+ const isArgument = function(argument1, argument2) {
+    if (typeof(argument1)=== typeof(argument2)) {
+        if (argument1 >argument2)  {
+            return argument1;
+        }
+        if (argument1<argument2) {
+            return argument2;
+        } 
+        else {
+            return null;
+        }
+    }
+    else {
+            return null;
+    }    
+ }
+ console.log(isArgument(6,3))
