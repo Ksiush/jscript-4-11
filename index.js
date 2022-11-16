e20fe443e04e71b0bd372452707c993ce684f98e
Xenia Golovach <ksiushagolovach@gmail.com>
2022-11-16 19:57:41 +0200

    Add homework 15-11

--- index.js
+++ index.js
@@ -1,71 +1,35 @@
-// while(true){
-//     const PIN_CODE ='1111'
-// }
 
-// Запитувати у користувача число до тих пір, доки воно не буде більше 15 і менше 35 і кратне 6 (18,24,30 підходить)
-// В користувача є 3 спроби
+const Start_diapazon = 0;
+const End_diapazon=50;
+const enterNumber=function(Start_diapazon=0, End_diapazon=50){
+    while(true) {
+        const input = prompt('Enter a number');
+    if(input !=='' && input!==' ' && input!==null){
+        if(input>Start_diapazon && input<End_diapazon){
+            return Number(input)
 
-// зробити двома способами через while і for
-
-// * це повинна бути функція(можливо і не одна)
-
-// const numb1 = (18|24,30)
-let count = 0;
-while (true) {
-  const result = Number(prompt("Введіть число, більше 15, менше 35, кратне 6d"));
-  count++;
-
-  if (result > 15 && result < 35 && result % 6 === 0) {
-    console.log("You win");
-    break;
-  }
-  if (count >= 3) {
-    console.log("try again");
-    break;
-  }
+        }
+    }
 }
-
-const validate = function () {
-  for (let i = 0; i < 3; i++) {
-    const result = prompt("Введіть число, більше 15, менше 35, кратне 6c");
-    if (result > 15 && result < 35 && result % 6 === 0) {
-      return 'You win';
-    }
-  }
-  return 'Try again';
-};
-console.log(validate());
-
-
-const isValid = function(number){
-    if (number > 15 && number < 35 && number % 6 === 0) {
-        return true;
-    }
-    return false;
 }
+console.log(enterNumber(3,49))
 
-for (let i = 0; i < 3; i++) {
-    const result = prompt("Введіть число, більше 15, менше 35, кратне 6b");
-    if (isValid(result)){
-        console.log('You win');
-    }
-    if (i==3){
-        console.log('You loss');
-        break;
+
+
+
+
+function user (firstName, lastName, userAge){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.userAge = userAge;
+    this.fullName = function(){
+        console.log(this.firstName + this.lastName)
     }
 }
 
-let condition = true;
-while (!condition) {
-    const n = prompt("Введіть число, більше 15, менше 35, кратне 6 a");
-    condition = isValid(n);
-}
-console.log('You win');
+const name = new user ('Xenia', 'Golovach', 24);
+console.log(name)
+name.fullName()
 
 
-// *
-// **
-// ***
-// ****
-// *****
-// ******
+
