befcc209ae18ec81e6448857ea65a6c44a8e2b6b
Xenia Golovach <ksiushagolovach@gmail.com>
2022-11-18 21:17:46 +0200

    Add homework proto

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-
-
+// 1 
 function Shoes(name, price, currensy ,discount){
     this.name = name;
     this.price =price;
@@ -11,8 +9,8 @@ function Shoes(name, price, currensy ,discount){
     if (this.price>100){
             this.price=this.price - this.discount;
             return this.price+this.currensy ;
-        } 
-        return 'try again'    
+        }
+        return 'try again'
  }
  Shoes.prototype.info= function(){
     return `${this.name}, ${this.price}, ${this.currensy}`
@@ -20,3 +18,42 @@ function Shoes(name, price, currensy ,discount){
 const gucci= new Shoes('gucci', 1000, '$');
 const gacci= new Shoes('gacci', 100, '$');
 
+// 2
+
+function Shoes(name, price, currensy, discount) {
+  this.name = name;
+  this.price = price;
+  this.currensy = currensy;
+  this.discount = 50;
+  this.info = function () {
+    return `${this.name}, ${this.price}, ${this.currensy}`;
+  };
+  this.addDiscounts = function () {
+    if (this.price > 100) {
+      this.price = this.price - this.discount;
+      return this.price + this.currensy;
+    }
+    return "try again";
+  };
+}
+// const gucci = new Shoes("gucci", 1000, "$");
+// const gacci = new Shoes("gacci", 100, "$");
+
+
+
+// 3
+function Shoes(name, price, currensy ,discount){
+         this.name = name;
+         this.price =price;
+         this.currensy =currensy;
+         this.discount = 50;
+     }
+      Shoes.prototype.discounts =function(){
+         if (this.price>100){
+                 this.price=this.price - this.discount;
+                return this.price+this.currensy ;
+             }
+           return 'try again'
+      }
+    //  const gucci= new Shoes('gucci', 1000, '$');
+    //  const gacci= new Shoes('gacci', 100, '$');
