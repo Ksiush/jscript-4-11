26791f4b6c02212b77cbdd80886b3a9baff9e100
Xenia Golovach <ksiushagolovach@gmail.com>
2022-11-27 17:27:15 +0200

    Add homework from 21-11

--- MyArray.js
+++ MyArray.js
@@ -1,25 +1,20 @@
-// 1) Знайти середнє арифметичне значення усіх переданих аргументів, якщо аргументів не має - повертати null
+// написати скрипт (можна функцію можна без), який виводить індекс максимального елемента в масиві. Якщо максимальних елементів декілька(значення однакові) - виводить індекс останнього
+// const arrNums = [1, 9, 5, 6 , 7, 9, 4, 6]; => індекс 5
 
-
-
-const arMean = function() {
-  const args = Array.from(arguments);
-  const summ = args.reduce((sum, current) => 
-{
-  return sum + current;
-})
-return summ / arguments.length;
-};
-
-console.log(arMean(1,2,3,4,5,6));
-
-// 2) Замінити усі максимальні значення в масиві на значення 0
-
-const chMaxArr = [10,2,3,4,5,5,4,5,10];
-const max = Math.max.apply(null, chMaxArr);
-for (let i = 0; i <chMaxArr.length; i++) {
-  if (chMaxArr[i] === max){
-    chMaxArr[i] = 0;
-  }
+const getIndexOfMax = (arr) => {
+  const max = Math.max.apply(null, arr);
+  return arr.lastIndexOf(max);
 }
-console.log(chMaxArr);
+
+console.log(getIndexOfMax([1,19, 5.6,7,9,4,19]));
+
+// * повертати кількість максимальних(однакових) елементів
+// const arrNums = [1, 9, 5, 6 , 7, 9, 4, 6]; =>кількість  2
+
+
+const getCountMax = (arr) => {
+  arr.sort();
+  const result = arr.filter(el => el ===arr.slice(-1) [0]);
+  return result.lenght;
+}
+console.log(getCountMax([1,9,5,6,7,9,4,6]));
